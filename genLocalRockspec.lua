9f82a871d801002068da8c470bb1ee6dd435ebdb
Calvin Rose <calsrose@gmail.com>
2016-06-26 19:27:02 -0400

    Add script for generating a local rockspec for testing.

---
+++ genLocalRockspec.lua
@@ -0,0 +1,50 @@
+local format = [[
+package = "moonmint"
+version = "%s"
+source = {
+    url = "file:////0.0.0.0%s"
+}
+description = {
+    homepage = "https://github.com/bakpakin/moonmint",
+    summary = "Express like web framework for Lua",
+    license = "MIT",
+}
+dependencies = {
+    "lua >= 5.1",
+    "luv ~> 1.8",
+    "luv-coro-channel >= 1.8",
+    "luv-coro-net >= 1.8",
+    "mimetypes >= 1.0",
+    "bkopenssl >= 0.0",
+    "bit32"
+}
+build = {
+    type = "builtin",
+    modules = {
+        ["moonmint"] = "init.lua",
+
+        ["moonmint.server"] = "src/server.lua",
+        ["moonmint.static"] = "src/static.lua",
+        ["moonmint.util"] = "src/util.lua",
+        ["moonmint.router"] = "src/router.lua",
+        ["moonmint.template"] = "src/template.lua",
+        ["moonmint.fs"] = "src/fs.lua",
+
+        ["moonmint.deps.codec.http"] = "deps/codec/http.lua",
+        ["moonmint.deps.codec.tls"] = "deps/codec/tls.lua",
+        ["moonmint.deps.pathjoin"] = "deps/pathjoin.lua",
+        ["moonmint.deps.headers"] = "deps/headers.lua",
+        ["moonmint.deps.request"] = "deps/request.lua",
+        ["moonmint.deps.response"] = "deps/response.lua"
+    }
+}
+]]
+
+local currentDirectory = io.popen('pwd'):read'*l'
+local version = 'local-0'
+local target = ('moonmint-%s.rockspec'):format(version)
+
+local f = assert(io.open(target, 'w'))
+f:write(format:format(version, currentDirectory))
+f:close();
+print(('Wrote to %s.'):format(target))
