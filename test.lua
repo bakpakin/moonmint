bb31f4cc93db510e921039056daa7b0a61e3f568
Calvin Rose <calsrose@gmail.com>
2016-07-24 13:51:08 -0400

    Make test.lua not use fancy tricks for detecting tests.

--- test.lua
+++ test.lua
@@ -1,15 +1,16 @@
-
 -- Expose the test function globally
 test = require 'tools.oft'
 
+local tests = {
+    'html',
+    'query',
+    'simpleServer',
+    'url'
+}
+
 local res = test(function()
-    -- Get all of the files in the spec directory that end with
-    -- '_spec' Use moonmint fs because we already have it
-    local fs = require 'moonmint.fs'
-    for item in fs.sync.scandir('spec') do
-        if item.type == 'file' and item.name:match('^.*_spec%.lua$') then
-            require('spec.' .. item.name:gsub('%.lua$', ''))
-        end
+    for i, v in ipairs(tests) do
+        require('spec.' .. tests[i] .. '_spec')
     end
 end)
 
